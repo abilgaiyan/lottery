e2be6d941e2b7a6e4d11c0373b32b716543c2138
ajay bilgaiyan <abilgaiyan@gmail.com>
2018-02-15 09:44:46 +0530

    fix the error for provider testing js

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -2,7 +2,9 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+// UPDATE THESE TWO LINES RIGHT HERE!!!!! <-----------------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -10,11 +12,13 @@ let lottery;
 let accounts;
 
 beforeEach( async () => {
+   // web3 = new Web3(ganache.provider());
   accounts = await web3.eth.getAccounts();
  
   lottery = await new web3.eth.Contract(JSON.parse(interface))
            .deploy({data: bytecode})
            .send({from: accounts[0], gas: '1000000'});
+  lottery.setProvider(provider) ;          
 });
 
 describe('Lottery Contract', () => {
@@ -40,6 +44,42 @@ describe('Lottery Contract', () => {
        assert.equal(1, players.length);
 
     });
+
+    it('Allows multiple account to enter', async ()  =>{
+       
+        await lottery.methods.enter().send({
+          from: accounts[0],
+          value: web3.utils.toWei('0.02', 'ether')
+         //value: '20000000000000000'
+
+       });
+
+       await lottery.methods.enter().send({
+        from: accounts[1],
+        value: web3.utils.toWei('0.02', 'ether')
+       //value: '20000000000000000'
+
+     });
+
+     await lottery.methods.enter().send({
+        from: accounts[2],
+        value: web3.utils.toWei('0.02', 'ether')
+       //value: '20000000000000000'
+
+     });
+
+
+       const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+       });
+
+       assert.equal(players[0], accounts[0]);
+       assert.equal(players[1], accounts[1]);
+       assert.equal(players[2], accounts[2]);
+       assert.equal(3, players.length);
+
+    });
+
 
 });
 
